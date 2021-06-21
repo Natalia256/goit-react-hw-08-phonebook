649136bc06a4238d1503fdb9a0d183c1a3bd642e
Natalia256 <70845659+Natalia256@users.noreply.github.com>
2021-06-21 19:30:55 +0300

    Updates

---
+++ static/js/4.6ceedb03.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[4],{93:function(e,o,t){"use strict";t.r(o);var c=t(33),n=t(1);o.default=function(e){return Object(n.jsx)(c.a,{children:Object(n.jsx)("div",{className:"homeView",children:"Welcome on board"})})}}}]);
+//# sourceMappingURL=4.6ceedb03.chunk.js.map
