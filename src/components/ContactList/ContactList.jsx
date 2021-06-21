733fcd137780665adbafcd3dfa0a173db5363cba
Natalia256 <70845659+Natalia256@users.noreply.github.com>
2021-06-21 19:34:58 +0300

    Add files via upload

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,34 @@
+import ContactItem from '../ContactItem'
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import operations from "../../redux/phonebook/operations";
+import selectors from "../../redux/phonebook/selectors";
+
+const ContactList = ({ list, onClick }) => {
+  return ((list.length ?
+    (
+    <ul>
+      {list.map(item => (
+        <ContactItem
+          key={item.id}
+          id={item.id}
+          onClick={onClick}
+          name={item.name}
+          number={item.number} />
+      ))
+      }
+    </ul>
+  ):(<p>No matches found</p>)))
+}
+ContactList.propTypes = {
+  list: PropTypes.array.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  list: selectors.getVisibleContacts(state),
+});
+const mapDispatchToProps = (dispatch) => ({
+  onClick: (id) => dispatch(operations.deleteContact(id)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(ContactList);;
---
+++ src/views/HomeView/HomeView.jsx
@@ -0,0 +1,15 @@
+  
+// import PropTypes from 'prop-types';
+import Container from "../../components/Container/Container";
+
+const HomeView = (props) => {
+  return (
+    <Container>
+      <div className="homeView">Welcome on board</div>
+    </Container>
+  );
+};
+
+HomeView.propTypes = {};
+
+export default HomeView;
